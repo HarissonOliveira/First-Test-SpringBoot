85e5bdc7e17f5b63a39f2ab9fc3ffdbcd2b58fb1
Harisson Oliveira <harisson.batista.oliveira@everis.com>
2021-09-16 18:53:24 -0300

    Added PathVariable to getClient

--- src/main/java/br/com/harisson/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/harisson/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.harisson.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.harisson.exerciciossb.models.Cliente;
@@ -13,6 +14,11 @@ public class ClienteController {
 	@GetMapping("/qualquer")
 	public Cliente getCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente getById(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321-00");
 	}
 
 }
